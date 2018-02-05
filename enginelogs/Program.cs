298ceecee9d6cea1d41c2d964800751db5674f89
Frandi Dwi <dev@rumahfrandi.com>
2018-02-05 15:18:09 +0700

    Implement SimpleProcessor

---
+++ enginelogs/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Azure.EventHubs;
+using Microsoft.Azure.EventHubs.Processor;
+
+namespace enginelogs
+{
+    class Program
+    {
+        private const string EhConnectionString = "";
+        private const string EhEntityPath = "engine-logs";
+        private const string StorageContainerName = "engine-logs";
+        private const string StorageAccountName = "catapultrelease";
+        private const string StorageAccountKey = "";
+
+        private static readonly string StorageConnectionString = string.Format("DefaultEndpointsProtocol=https;AccountName={0};AccountKey={1}", StorageAccountName, StorageAccountKey);
+
+        static void Main(string[] args)
+        {
+            MainAsync(args).GetAwaiter().GetResult();
+        }
+
+        private static async Task MainAsync(string[] args)
+        {
+            Console.WriteLine("Registering EventProcessor...");
+
+            var eventProcessorHost = new EventProcessorHost(
+                EhEntityPath,
+                PartitionReceiver.DefaultConsumerGroupName,
+                EhConnectionString,
+                StorageConnectionString,
+                StorageContainerName);
+
+            // Registers the Event Processor Host and starts receiving messages
+            await eventProcessorHost.RegisterEventProcessorAsync<SimpleProcessor>();
+
+            Console.WriteLine("Receiving. Press ENTER to stop worker.");
+            Console.ReadLine();
+
+            // Disposes of the Event Processor Host
+            await eventProcessorHost.UnregisterEventProcessorAsync();
+        }
+    }
+}
